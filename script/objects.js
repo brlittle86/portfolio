8349b5b6a447dd2029962fc69fec06cb4d4d5423
Brandon Little <brlittle86@gmail.com>
2017-02-08 10:54:14 -0800

    added test object

--- script/objects.js
+++ script/objects.js
@@ -1,1 +1,5 @@
 'use strict';
+
+var projects = [
+  new Project('About Me', 'A small introduction to me as a new coder.', 'This assignment was my first project, which required only the use of HTML, CSS, and JavaScript to tell and show a little about myself.', 'January 4, 2017', 'January 8, 2017')
+]
