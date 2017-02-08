e917055d2c16c8d26a2ea9dba19aed0d1592913f
Brandon Little <brlittle86@gmail.com>
2017-02-08 10:49:31 -0800

    created basic constructor for Project objects

--- script/constructors.js
+++ script/constructors.js
@@ -1,1 +1,10 @@
 'use strict'
+
+//Constructor function for project objects
+function Project(title, shortDescription, longDescription, dateCreated, dateCompleted) {
+  this.title = title;
+  this.shortDescription = shortDescription;
+  this.longDescription = longDescription;
+  this.dateCreated = dateCreated;
+  this.dateCompleted = dateCompleted;
+}
