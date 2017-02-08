fde1c0e6011edf370e7dac200d3d3fc0d337dc9b
Brandon Little <brlittle86@gmail.com>
2017-02-08 11:08:57 -0800

    edit for semicolon

--- script/objects.js
+++ script/objects.js
@@ -2,4 +2,4 @@
 
 var projects = [
   new Project('About Me', 'A small introduction to me as a new coder.', 'This assignment was my first project, which required only the use of HTML, CSS, and JavaScript to tell and show a little about myself.', 'January 4, 2017', 'January 8, 2017')
-]
+];
