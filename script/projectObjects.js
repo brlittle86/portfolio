67368b89eafb45e201ea5da34ee03e553d50af3d
brlittle86 <brlittle86@gmail.com>
2017-02-12 12:56:52 -0800

    major refactor in approach

--- script/projectObjects.js
+++ script/projectObjects.js
@@ -4,21 +4,21 @@ var projectData = [
   {
     title: 'About Me',
     shortDescription: 'A small introduction to me as a new coder.',
-    longDescription: 'This assignment was my first project, which required only the use of HTML, CSS, and JavaScript to tell and show a little about myself.',
+    longDescription: '<p>This assignment was my first project, which required only the use of HTML, CSS, and JavaScript to tell and show a little about myself.</p>',
     createdOn: '2017-01-04',
     completedOn: '2017-01-08'
   },
   {
     title: 'Sample Project 2',
     shortDescription: 'An exercise emphasizing CSS.',
-    longDescription: 'A longer description talking about certain challenges met during the project.',
+    longDescription: '<p>A longer description talking about certain challenges met during the project.</p>',
     createdOn: '2017-01-10',
     completedOn: '2017-01-12'
   },
   {
     title: 'Sample Project 3',
     shortDescription: 'A project using nested for loops.',
-    longDescription: 'For loops should be nested very carefully or they could result in infinite loops.',
+    longDescription: '<p>For loops should be nested very carefully or they could result in infinite loops.</p>',
     createdOn: '2017-01-15',
     completedOn: '2017-01-20'
   }
