d63997b30e9016caa6f705ceecb64e2bb86edafb
brlittle86 <brlittle86@gmail.com>
2017-02-12 11:57:15 -0800

    altered data format

--- script/objects.js
+++ script/objects.js
@@ -1,25 +1,25 @@
 'use strict';
 
 var projectData = [
-  [
-    'About Me',
-    'A small introduction to me as a new coder.',
-    'This assignment was my first project, which required only the use of HTML, CSS, and JavaScript to tell and show a little about myself.',
-    '2017-01-04',
-    '2017-01-08'
-  ],
-  [
-    'Sample Project 2',
-    'An exercise emphasizing CSS.',
-    'A longer description talking about certain challenges met during the project.',
-    '2017-01-10',
-    '2017-01-12'
-  ],
-  [
-    'Sample Project 3',
-    'A project using nested for loops.',
-    'For loops should be nested very carefully or they could result in infinite loops.',
-    '2017-01-15',
-    '2017-01-20'
-  ]
+  {
+    title: 'About Me',
+    shortDescription: 'A small introduction to me as a new coder.',
+    longDescription: 'This assignment was my first project, which required only the use of HTML, CSS, and JavaScript to tell and show a little about myself.',
+    createdOn: '2017-01-04',
+    completedOn: '2017-01-08'
+  },
+  {
+    title: 'Sample Project 2',
+    shortDescription: 'An exercise emphasizing CSS.',
+    longDescription: 'A longer description talking about certain challenges met during the project.',
+    createdOn: '2017-01-10',
+    completedOn: '2017-01-12'
+  },
+  {
+    title: 'Sample Project 3',
+    shortDescription: 'A project using nested for loops.',
+    longDescription: 'For loops should be nested very carefully or they could result in infinite loops.',
+    createdOn: '2017-01-15',
+    completedOn: '2017-01-20'
+  }
 ];
