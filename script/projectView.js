0bd0fff7f13af163796f699f40721becb158e5ad
brlittle86 <brlittle86@gmail.com>
2017-02-12 13:22:33 -0800

    added handler for main nav

--- script/projectView.js
+++ script/projectView.js
@@ -1,1 +1,11 @@
 'use strict';
+
+var projectView = {};
+
+articleView.handleMainNav = function () {
+  $('.main-nav').on('click', '.tab', function () {
+    $('.tab-content').hide();
+    $('#' + $(this).data('content')).fadeIn();
+  });
+  $('.main-nav .tab:first').click();
+};
