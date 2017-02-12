04beb41f5875b9163bdc4b976b006fe4ed74b28d
brlittle86 <brlittle86@gmail.com>
2017-02-12 11:46:14 -0800

    added template and sample project data

--- script/objects.js
+++ script/objects.js
@@ -1,5 +1,25 @@
 'use strict';
 
-var projects = [
-  new Project('About Me', 'A small introduction to me as a new coder.', 'This assignment was my first project, which required only the use of HTML, CSS, and JavaScript to tell and show a little about myself.', 'January 4, 2017', 'January 8, 2017')
+var projectData = [
+  [
+    'About Me',
+    'A small introduction to me as a new coder.',
+    'This assignment was my first project, which required only the use of HTML, CSS, and JavaScript to tell and show a little about myself.',
+    '2017-01-04',
+    '2017-01-08'
+  ],
+  [
+    'Sample Project 2',
+    'An exercise emphasizing CSS.',
+    'A longer description talking about certain challenges met during the project.',
+    '2017-01-10',
+    '2017-01-12'
+  ],
+  [
+    'Sample Project 3',
+    'A project using nested for loops.',
+    'For loops should be nested very carefully or they could result in infinite loops.',
+    '2017-01-15',
+    '2017-01-20'
+  ]
 ];
