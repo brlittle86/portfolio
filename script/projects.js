f88a25f1c68d9d4862a61261b192c642eff3b8e4
Brandon Little <brlittle86@gmail.com>
2017-02-15 09:12:31 -0800

    started converting code to load data from JSON file

--- script/projects.js
+++ script/projects.js
@@ -2,7 +2,7 @@
 
 var projects = [];
 
-function Project (opts) {
+function Project(opts) {
   this.title = opts.title;
   this.createdOn = opts.createdOn;
   this.completedOn = opts.completedOn;
@@ -10,25 +10,29 @@ function Project (opts) {
   this.longDescription = opts.longDescription;
 }
 
-Project.prototype.toHtml = function() {
+Project.all = [];
+
+Project.prototype.toHtml = function () {
   var source = $('#project-template').html();
   var templateRender = Handlebars.compile(source);
 
-  this.daysAgo = parseInt((new Date() - new Date(this.createdOn))/60/60/24/1000);
+  this.daysAgo = parseInt((new Date() - new Date(this.createdOn)) / 60 / 60 / 24 / 1000);
   this.publishStatus = this.createdOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
   return templateRender(this);
 
 };
 
-projectData.sort(function(a,b) {
-  return (new Date(b.createdOn)) - (new Date(a.createdOn));
-});
+Article.loadAll = function (projectData) {
+  projectData.sort(function (a, b) {
+    return (new Date(b.createdOn)) - (new Date(a.createdOn));
+  });
 
-projectData.forEach(function(projectObject) {
-  projects.push(new Project(projectObject));
-});
+  projectData.forEach(function (projectObject) {
+    projects.push(new Project(projectObject));
+  });
+}
 
-projects.forEach(function(a) {
+projects.forEach(function (a) {
   $('#projects').append(a.toHtml());
 });
