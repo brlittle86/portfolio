553134bdcbf59574833649af725c7b4487adf45b
Brandon Little <brlittle86@gmail.com>
2017-02-21 10:19:43 -0800

    wrapped in an IIFE

--- public/script/projects.js
+++ public/script/projects.js
@@ -1,51 +1,55 @@
 'use strict';
 
-var projects = [];
+(function (module) {
+  var projects = [];
 
-function Project(opts) {
-  this.title = opts.title;
-  this.createdOn = opts.createdOn;
-  this.completedOn = opts.completedOn;
-  this.shortDescription = opts.shortDescription;
-  this.longDescription = opts.longDescription;
-}
+  function Project(opts) {
+    this.title = opts.title;
+    this.createdOn = opts.createdOn;
+    this.completedOn = opts.completedOn;
+    this.shortDescription = opts.shortDescription;
+    this.longDescription = opts.longDescription;
+  }
 
-Project.all = [];
+  Project.all = [];
 
-Project.prototype.toHtml = function () {
-  var source = $('#project-template').html();
-  var templateRender = Handlebars.compile(source);
+  Project.prototype.toHtml = function () {
+    var source = $('#project-template').html();
+    var templateRender = Handlebars.compile(source);
 
-  this.daysAgo = parseInt((new Date() - new Date(this.createdOn)) / 60 / 60 / 24 / 1000);
-  this.publishStatus = this.createdOn ? `published ${this.daysAgo} days ago` : '(draft)';
+    this.daysAgo = parseInt((new Date() - new Date(this.createdOn)) / 60 / 60 / 24 / 1000);
+    this.publishStatus = this.createdOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
-  return templateRender(this);
+    return templateRender(this);
 
-};
+  };
 
-Project.loadAll = function (projectData) {
-  projectData.sort(function (a, b) {
-    return (new Date(b.createdOn)) - (new Date(a.createdOn));
-  });
+  Project.loadAll = function (projectData) {
+    projectData.sort(function (a, b) {
+      return (new Date(b.createdOn)) - (new Date(a.createdOn));
+    });
 
-  projectData.forEach(function (projectObject) {
-    Project.all.push(new Project(projectObject));
-  });
-}
+    projectData.forEach(function (projectObject) {
+      Project.all.push(new Project(projectObject));
+    });
+  }
 
-Project.fetchAll = function() {
-  if (localStorage.rawData) {
-    Project.loadAll(JSON.parse(localStorage.rawData));
-    projectView.initIndexPage();
-  } else {
-    $.getJSON('data/projectObjects.json')
-      .then(
-        function(data) {
+  Project.fetchAll = function () {
+    if (localStorage.rawData) {
+      Project.loadAll(JSON.parse(localStorage.rawData));
+      projectView.initIndexPage();
+    } else {
+      $.getJSON('data/projectObjects.json')
+        .then(
+        function (data) {
           localStorage.setItem('rawData', JSON.stringify(data));
           Project.loadAll(data);
           projectView.initIndexPage();
-        }, function(error){
+        }, function (error) {
           console.log('There was an error:', error);
         })
+    }
   }
-}
+
+  module.Project = Project;
+})(window);
