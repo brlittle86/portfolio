739b45f2051800ca6ddc3421fa869052a71ca114
Brandon Little <brlittle86@gmail.com>
2017-02-21 10:38:29 -0800

    edit white space

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const PORT = process.env.PORT || 8686;
 app.use(express.static('./public'));
 
 app.get('', (request, response) => {
-  response.sendFile('index.html', { root: './public'});
+  response.sendFile('index.html', { root: './public' });
 });
 
 app.listen(PORT, () => {
-  console.log('Our amazing server app is being served at localhost:'+PORT);
+  console.log('Our amazing server app is being served at localhost:' + PORT);
 });
