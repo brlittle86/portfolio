a4bb2c334e5b90b20f67cc9f63e3de49922a7bd7
Brandon Little <brlittle86@gmail.com>
2017-02-22 09:26:49 -0800

    added controller function

--- public/scripts/controllers/projectController.js
+++ public/scripts/controllers/projectController.js
@@ -1,1 +1,12 @@
 'use strict';
+
+(function (module) {
+  const aboutController = {};
+
+  aboutController.init = () => {
+    $('#projects').hide();
+    $('#about').show();
+  }
+
+  module.aboutController = aboutController;
+})(window);
