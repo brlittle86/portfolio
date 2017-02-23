eaf58222a866847528e5ad5cbbac07992e826a9b
Brandon Little <brlittle86@gmail.com>
2017-02-23 09:29:09 -0800

    removed conflicted code

--- server.js
+++ server.js
@@ -8,17 +8,11 @@ const PORT = process.env.PORT || 8686;
 app.use(express.static('./public'));
 
 app.get('', (request, response) => {
-<<<<<<< HEAD
+
   response.sendFile('index.html', { root: './public' });
 });
 
 app.listen(PORT, () => {
   console.log('Our amazing server app is being served at localhost:' + PORT);
-=======
-  response.sendFile('index.html', { root: './public'});
-});
 
-app.listen(PORT, () => {
-  console.log('Our amazing server app is being served at localhost:'+PORT);
->>>>>>> 94f5ec41d2f906c12ae2837a017f6134a01542ce
 });
