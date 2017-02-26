9e1b7788728c9660e6a0c7fb324b217203ef18cc
brlittle86 <brlittle86@gmail.com>
2017-02-25 16:48:59 -0800

    removed comments

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -6,9 +6,8 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    // DONE: Refactor your ajax call to use the $.get method, and make a request to our new proxy route.
     $.get('github/user/repos')
-    .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
+    .then(data => repos.all = data, err => console.error(err))
     .then(callback);
   };
 
